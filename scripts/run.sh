e36402231c943869990f8542f689ecf191394e0a
Charles Mulder <7zh6dj9f9@mozmail.com>
2022-08-18 16:39:50 +0100

    fix shell script quotes

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,11 @@ set -e
 
 dockerize -timeout 300s -wait tcp://$DB_HOST:3306 
 
-if ! wp core is-installed; then
-    runuser -s /bin/bash -c "wp core install --url=$SITE_URL --title='$SITE_TITLE' --admin_user=$ADMIN_USERNAME --admin_email=$ADMIN_EMAIL --admin_password=$ADMIN_PASSWORD" www-data
-    runuser -s /bin/bash -c "wp plugin install $WP_PLUGINS --activate" www-data
-    runuser -s /bin/bash -c "wp theme install $WP_THEME --activate" www-data
-    runuser -s /bin/bash -c "wp rewrite structure --hard '/%postname%/'" www-data
+if ! runuser -s /bin/sh -c 'wp core is-installed' www-data; then
+    runuser -s /bin/sh -c 'wp core install --url=$SITE_URL --title="$SITE_TITLE" --admin_user=$ADMIN_USERNAME --admin_email=$ADMIN_EMAIL --admin_password=$ADMIN_PASSWORD' www-data
+    runuser -s /bin/sh -c 'wp plugin install $WP_PLUGINS --activate' www-data
+    runuser -s /bin/sh -c 'wp theme install $WP_THEME --activate' www-data
+    runuser -s /bin/sh -c 'wp rewrite structure --hard "/%postname%/"' www-data
 fi
 
 apache2-foreground
