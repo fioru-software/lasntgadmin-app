86678aec70900cb7f820902126038511d153e34a
cm <7zh6dj9f9@mozmail.com>
2024-01-15 11:26:07 +0000

    Security improvements
    prevent user enum attack
    
    prefix
    
    htaccess configmap fix
    
    prevent user name enum
    
    wordfence
    
    wordfence waf
    
    updates
    
    path vulnerability fix
    
    updates
    
    path vulnerability fix
    
    lock plugins
    
    composer update
    
    remove plugins
    
    groups plugin update lock

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -6,3 +6,4 @@ runuser -s /bin/sh -c 'wp plugin activate $WP_PLUGINS' www-data
 if runuser -s /bin/sh -c 'wp plugin is-installed hyperdb' www-data; then
     runuser -s /bin/sh -c 'ln -s /var/www/html/wp-content/plugins/hyperdb/db.php /var/www/html/wp-content/db.php' www-data
 fi
+e
