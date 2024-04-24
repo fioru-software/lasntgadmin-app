f79dce1b5194e1286749e86f649ae703299a100a
cm <7zh6dj9f9@mozmail.com>
2024-04-24 12:49:41 +0100

    remove deploy magic

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,14 @@
 
 dir="/usr/local/bin"
 $dir/install.sh # install wp 
-$dir/groups.sh # create groups
-$dir/funding_sources.sh # create funding sources for grant funded payment gateway
-$dir/roles.sh # create user roles
-$dir/caps.sh # create user roles
+#$dir/groups.sh # create groups
+#$dir/funding_sources.sh # create funding sources for grant funded payment gateway
+#$dir/roles.sh # create user roles
+#$dir/caps.sh # create user roles
 $dir/composer_install.sh
-$dir/plugins.sh # install plugins
-$dir/product_cat.sh #create product categories
-$dir/themes.sh # install themes
+#$dir/plugins.sh # install plugins
+#$dir/product_cat.sh #create product categories
+#$dir/themes.sh # install themes
 
 if [ $# -eq 0 ]; then
     service cron start
