942478a15692570f072c25e13e61bcbeeadffaa1
cm <7zh6dj9f9@mozmail.com>
2025-01-07 20:20:54 +0000

    nightly cache flush

--- scripts/housekeeper.sh
+++ scripts/housekeeper.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
-/usr/local/bin/php /usr/local/bin/wp action-scheduler clean --status=complete,failed,canceled --before='14 days ago' --pause=1 --path=/var/www/html
+/usr/local/bin/php /usr/local/bin/wp action-scheduler clean --status=complete,failed,canceled --before='7 days ago' --pause=1 --path=/var/www/html
 /usr/local/bin/php /usr/local/bin/wp db optimize --path=/var/www/html
+/usr/local/bin/php /usr/local/bin/wp cache flush --path=/var/www/html
