dee8d64e14c2311f78e1e7225cd61ec388577cd1
cm <7zh6dj9f9@mozmail.com>
2025-01-07 21:05:46 +0000

    missing env in housekeeper script

--- scripts/housekeeper.sh
+++ scripts/housekeeper.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+export COMPOSER_AUTOLOAD_FILEPATH=/var/www/html/vendor/autoload.php
+
 /usr/local/bin/php /usr/local/bin/wp action-scheduler clean --status=complete,failed,canceled --before='7 days ago' --pause=1 --path=/var/www/html
 /usr/local/bin/php /usr/local/bin/wp db optimize --path=/var/www/html
 /usr/local/bin/php /usr/local/bin/wp cache flush --path=/var/www/html
