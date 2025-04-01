9a2488de8683df8d28a18f9ac59af273ee739d2f
cm <7zh6dj9f9@mozmail.com>
2025-04-01 16:15:22 +0100

    action scheduler group param requires migration

--- cron/everyminute.sh
+++ cron/everyminute.sh
@@ -2,4 +2,6 @@
 
 export COMPOSER_AUTOLOAD_FILEPATH=/var/www/html/vendor/autoload.php
 
+# the group param requires migration to be run first
+/usr/local/bin/php /usr/local/bin/wp action-scheduler run --hooks=action_scheduler/migration_hook --path=/var/www/html
 /usr/local/bin/php /usr/local/bin/wp action-scheduler run --group=woocommerce-db-updates --path=/var/www/html
